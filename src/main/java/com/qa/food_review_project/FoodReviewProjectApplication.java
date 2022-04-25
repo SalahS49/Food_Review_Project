04ff891d5f685190854b8443d9f6703e55f1dc76
SalahS49 <salah_49@outlook.com>
2022-04-25 17:05:39 +0100

    initial commit

---
+++ src/main/java/com/qa/food_review_project/FoodReviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.food_review_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodReviewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodReviewProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/food_review_project/FoodReviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.food_review_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodReviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
